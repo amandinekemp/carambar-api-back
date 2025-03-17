8d3ccdf3411d1b3741e2ff91b911c0e7ddf0b1a3
DDoudine <amandinekemp@gmail.com>
2025-03-17 10:20:14 +0100

    Add getJokeById

--- src/controllers/jokeController.js
+++ src/controllers/jokeController.js
@@ -18,6 +18,19 @@ exports.getAllJokes = async (req, res) => {
     res.json(jokes);
 };
 
+exports.getJokeById = async (req, res) => {
+    try {
+        const jokeId = req.params.id;
+        const joke = await Joke.findByPk(jokeId);
+        if (!joke) {
+            return res.status(404).json({ error: "Blague non trouvée" });
+        }
+        res.json(joke);
+    } catch (error) {
+        res.status(500).json({ error: "Erreur lors de la récupération de la blague" });
+    }
+};
+
 exports.getRandomJoke = async (req, res) => {
     const jokes = await Joke.findAll();
     if (jokes.length === 0) {
